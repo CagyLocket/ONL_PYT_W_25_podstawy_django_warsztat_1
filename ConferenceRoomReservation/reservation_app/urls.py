bdf7e1ef8cec84195a76d9309af32e06cde3b204
pawelwrzesien <rsc.pawel@gmail.com>
2023-05-13 17:12:59 +0200

    reservation app finished

--- ConferenceRoomReservation/reservation_app/urls.py
+++ ConferenceRoomReservation/reservation_app/urls.py
@@ -4,6 +4,13 @@ from . import views as reservation_views
 app_name = 'reservation_app'
 
 urlpatterns = [
-    path('', reservation_views.home_view, name="home"),
+    path('home/', reservation_views.home_view, name="home"),
+    path('room/new/', reservation_views.AddRoomView.as_view(), name="add-room"),
+    path('', reservation_views.RoomListView.as_view(), name="room-list"),
+    path('room/delete/<int:room_id>/', reservation_views.DeleteRoomView.as_view(), name="delete-room"),
+    path('room/modify/<int:room_id>/', reservation_views.ModifyRoomView.as_view(), name="modify-room"),
+    path('room/book/<int:room_id>/', reservation_views.ReservationView.as_view(), name="book-room"),
+    path('room/<int:room_id>/', reservation_views.RoomDetailsView.as_view(), name="room"),
+    path('search/', reservation_views.SearchView.as_view(), name="room-list"),
 
 ]
